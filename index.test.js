ba02725621a4522e3644c01757d3bc299c04ab17
DavidV05 <20220531@itla.edu.do>
2023-12-16 20:43:38 -0400

    pull request a la rama main

---
+++ index.test.js
@@ -0,0 +1,17 @@
+// index.test.js
+
+describe('Test Hola Mundo', () => {
+    test('should pass', () => {
+      if (typeof document !== 'undefined') {
+        const body = document.createElement('body');
+        const h1 = document.createElement('h1');
+        h1.textContent = 'Hola Mundo';
+        body.appendChild(h1);
+  
+        expect(document.body.textContent).toContain('Hola Mundo');
+      } else {
+
+        console.warn('No se pudo acceder al objeto document. Asegúrate de que estás ejecutando en un entorno adecuado.');
+      }
+    });
+  });
